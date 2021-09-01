ec2fc07e5b6acd7d41cf8331837d85d75e3eca03
cGIfl300 <cGIfl300@gmail.com>
2021-09-01 10:13:22 +0200

    Adding stars

---
+++ ticket/stars.py
@@ -0,0 +1,20 @@
+def stars(number_of_stars, max_stars):
+    """
+    For a given number of stars, will return the html code.
+    :param number_of_stars: number of stars
+    :param max_stars: max number of stars
+    :return: an HTML string
+    """
+
+    star_true = "★"
+    star_false = "☆"
+    return star_true * number_of_stars + star_false * (
+            max_stars - number_of_stars)
+
+
+def test_stars():
+    assert stars(0, 6) == "☆☆☆☆☆☆"
+    assert stars(1, 6) == "★☆☆☆☆☆"
+    assert stars(3, 6) == "★★★☆☆☆"
+    assert stars(5, 6) == "★★★★★☆"
+    assert stars(6, 6) == "★★★★★★"

ea99a4195fd74cf664498a90a008b66a3e0cd816
cGIfl300 <cgifl300@gmail.com>
2021-09-15 11:25:25 +0200

    Code reformatting

--- OCP09/urls.py
+++ OCP09/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
 
 from create_review.views import create_review_view
 from review.views import update_review

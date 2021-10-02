1230f5b6b7eeb865c03c84450af36d24af56ca63
cGIfl300 <cGIfl300@gmail.com>
2021-10-02 21:01:06 +0200

    Refresh static files served by whitenoise

--- OCP09/settings.py
+++ OCP09/settings.py
@@ -145,6 +145,6 @@ LOGIN_URL = "/login/"
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 if os.environ.get("ENV") == "PRODUCTION":
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES["default"].update(db_from_env)
